6a68dc655e8a60bccc9c1ffdd53f3489a3bdf738
Rizky Siregar <regarrezky@gmail.com>
2024-03-10 11:56:02 +0700

    Initial Commit

---
+++ MyiOSTodoList/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyiOSTodoList
+//
+//  Created by Rizky Siregar on 10/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyiOSTodoList/MyiOSTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyiOSTodoListApp.swift
+//  MyiOSTodoList
+//
+//  Created by Rizky Siregar on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MyiOSTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
